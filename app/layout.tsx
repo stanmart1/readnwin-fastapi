06c55d76f6a391c3ca6f053f5b221bb836459f24
stanmart1 <scaleitpro@gmail.com>
2025-08-26 00:23:16 +0100

    fixed linter errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { AuthProvider } from '@/contexts/AuthContext'
+import { AuthProvider } from '@/hooks/useAuth'
 import { CartProvider } from '@/contexts/CartContext'
 import { Toaster } from 'react-hot-toast'
 
