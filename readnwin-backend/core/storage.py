acdb7c2ea11e22ff559e35ac2c5b0f6718407c5b
stanmart1 <scaleitpro@gmail.com>
2025-11-07 14:05:57 +0100

    fixed blog image 06

--- readnwin-backend/core/storage.py
+++ readnwin-backend/core/storage.py
@@ -24,7 +24,7 @@ class StorageManager:
         
         if self.env == "production":
             self.base_dir = Path("/app/storage")
-            self.url_prefix = "/app/storage"
+            self.url_prefix = "/uploads"  # HTTP endpoint is always /uploads
         else:
             self.base_dir = Path("uploads")
             self.url_prefix = "/uploads"
